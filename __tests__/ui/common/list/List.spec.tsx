74ae5bdbff3edf32749fca5fb55f7fa86c6c70a7
Patrick Luzolo <eldorplus@gmail.com>
2020-05-20 15:39:58 +0200

    Merging

--- __tests__/ui/common/list/List.spec.tsx
+++ __tests__/ui/common/list/List.spec.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {
-  View as ListItem,
-  ViewProps,
   Image,
   ImageProps,
   ImageSourcePropType,
@@ -23,47 +21,256 @@ import {
   darkTheme,
   List,
   ListProps,
-  // ListItem,
-  // ListItemProps,
+  ListItem,
+  ListItemProps,
 } from '@southem/ui';
 
 Theme.registerDefaultTheme(darkTheme);
 
 const data: any[] = Array(8);
 
-const Mock = (props?: ListProps): React.ReactElement<ThemeProviderProps> => {
-  return (
-    <ThemeProvider
-      theme={'default'}>
-      <List {...props} />
-    </ThemeProvider>
-  );
-};
+describe('@list-item: component checks', () => {
+  // @ts-ignore
+  const ListMock = (props?: ListProps): React.ReactElement<ThemeProviderProps> => {
+    return (
+      <ThemeProvider
+        theme={'default'}>
+        <List {...props} />
+      </ThemeProvider>
+    );
+  };
+  const ListItemMock = (props?: ListItemProps): React.ReactElement<ListItemProps> => {
+    return (
+      <ThemeProvider
+        theme={'default'}>
+        <ListItem {...props} />
+      </ThemeProvider>
+    );
+  };
 
-const ItemMock = (props?: ViewProps): React.ReactElement<ViewProps> => {
-  return (
-    <ListItem {...props} />
-  );
-};
-/**
-const ItemMock = (props?: ListItemProps): React.ReactElement<ListItemProps> => {
-  return (
-    <ListItem {...props} />
-  );
-};
-**/
+  describe('* emits events with correct args', () => {
+    const pressIndex: number = 0;
 
-describe('@list: component checks', () => {
+    const onPress = jest.fn();
+    const onPressIn = jest.fn();
+    const onPressOut = jest.fn();
 
-  it('* renders proper amount of data', () => {
     const item = () => {
       return (
-        <ItemMock title='Title'/>
+        <ListItemMock
+          // @ts-ignore
+          title='Title'
+          onPress={onPress}
+          onPressIn={onPressIn}
+          onPressOut={onPressOut}
+        />
       );
     };
 
     const component: RenderAPI = render(
-      <Mock
+      <ListMock
+        data={data}
+        renderItem={item}
+      />,
+    );
+
+    it('should call onPress', async () => {
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const touchable: ReactTestInstance = items[pressIndex].findByType(TouchableOpacity);
+
+      fireEvent.press(touchable);
+
+      expect(onPress).toHaveBeenCalled();
+    });
+    it('should call onPressIn', async () => {
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const touchable: ReactTestInstance = items[pressIndex].findByType(TouchableOpacity);
+
+      fireEvent(touchable, 'pressIn');
+
+      expect(onPressIn).toHaveBeenCalled();
+    });
+    it('should call onPressOut', async () => {
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const touchable: ReactTestInstance = items[pressIndex].findByType(TouchableOpacity);
+
+      fireEvent(touchable, 'pressOut');
+
+      expect(onPressOut).toHaveBeenCalled();
+    });
+  });
+  describe('* template matches snapshot', () => {
+
+    const iconSource: ImageSourcePropType = { uri: 'https://i.imgur.com/0y8Ftya.jpg' };
+
+    it('* title', () => {
+      const item = () => {
+        return (
+          // @ts-ignore
+          <ListItemMock title='Title'/>
+        );
+      };
+
+      const component: RenderAPI = render(
+        <ListMock
+          data={data}
+          renderItem={item}
+        />,
+      );
+
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const { output } = shallow(items[0]);
+
+      expect(output).toMatchSnapshot();
+    });
+
+    it('* description', () => {
+      const item = () => {
+        return (
+          // @ts-ignore
+          <ListItemMock description='Description'/>
+        );
+      };
+
+      const component: RenderAPI = render(
+        <ListMock
+          data={data}
+          renderItem={item}
+        />,
+      );
+
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const { output } = shallow(items[0]);
+
+      expect(output).toMatchSnapshot();
+    });
+
+    it('* text styles', () => {
+      const item = () => {
+        return (
+          <ListItemMock
+            // @ts-ignore
+            title='Title'
+            titleStyle={{
+              fontSize: 22,
+              lineHeight: 24,
+            }}
+            description='Description'
+            descriptionStyle={{
+              color: 'blue',
+              letterSpacing: 6,
+            }}
+          />
+        );
+      };
+
+      const component: RenderAPI = render(
+        <ListMock
+          data={data}
+          renderItem={item}
+        />,
+      );
+
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const { output } = shallow(items[0]);
+
+      expect(output).toMatchSnapshot();
+    });
+
+    it('* with icon', () => {
+      const item = () => {
+        return (
+          <ListItemMock
+            // @ts-ignore
+            title='Title'
+            description='Description'
+            icon={icon}
+          />
+        );
+      };
+
+      const icon = (style: StyleType): React.ReactElement<ImageProps> => {
+        return (
+          <Image
+            style={style}
+            source={iconSource}
+          />
+        );
+      };
+
+      const component: RenderAPI = render(
+        <ListMock
+          data={data}
+          renderItem={item}
+        />,
+      );
+
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const { output } = shallow(items[0]);
+
+      expect(output).toMatchSnapshot();
+    });
+
+    it('* with accessory', () => {
+      const item = () => {
+        return (
+          <ListItemMock
+            // @ts-ignore
+            title='Title'
+            description='Description'
+            accessory={accessory}
+          />
+        );
+      };
+
+      const accessory = (style: StyleType): React.ReactElement<ImageProps> => {
+        return (
+          <Image
+            style={style}
+            source={iconSource}
+          />
+        );
+      };
+
+      const component: RenderAPI = render(
+        <ListMock
+          data={data}
+          renderItem={item}
+        />,
+      );
+
+      const items: ReactTestInstance[] = component.getAllByType(ListItem);
+      const { output } = shallow(items[0]);
+
+      expect(output).toMatchSnapshot();
+    });
+
+  });
+});
+
+describe('@list: component checks', () => {
+  // @ts-ignore
+  const ListMock = React.forwardRef((props: Partial<ListProps>, ref: React.Ref<List>) =>
+    <ThemeProvider
+      theme={'default'}>
+      <List
+        ref={ref}
+        data={new Array(2)}
+        renderItem={() => <ListItem />}
+        {...props}
+      />
+    </ThemeProvider>,
+  );
+  it('* renders proper amount of data', () => {
+    const item = () => {
+      return (
+        // @ts-ignore
+        <ListItem title='Title'/>
+      );
+    };
+
+    const component: RenderAPI = render(
+      <ListMock
         data={data}
         renderItem={item}
       />,
@@ -73,180 +280,56 @@ describe('@list: component checks', () => {
 
     expect(items.length).toEqual(8);
   });
-
-});
-describe('@list-item: template matches snapshot', () => {
-
-  const iconSource: ImageSourcePropType = { uri: 'https://i.imgur.com/0y8Ftya.jpg' };
-
-  it('* title', () => {
-    const item = () => {
-      return (
-        <ItemMock title='Title'/>
-      );
-    };
-
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
+  it('* should call renderItem once per visible item', () => {
+    const renderItem = jest.fn();
+    render(
+      <ListMock
+        data={new Array(11)}
+        renderItem={renderItem}
       />,
     );
 
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const { output } = shallow(items[0]);
-
-    expect(output).toMatchSnapshot();
+    expect(renderItem).toHaveBeenCalledTimes(10);
   });
+  it('* should be able to call scrollToEnd with ref', () => {
+    // @ts-ignore
+    const componentRef = React.createRef<List>();
 
-  it('* description', () => {
-    const item = () => {
-      return (
-        <ItemMock description='Description'/>
-      );
-    };
-
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
+    render(
+      <ListMock
+        // @ts-ignore
+        ref={componentRef}
+        data={new Array(11)}
       />,
     );
 
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const { output } = shallow(items[0]);
-
-    expect(output).toMatchSnapshot();
+    expect(componentRef.current.scrollToEnd).toBeTruthy();
+    componentRef.current.scrollToEnd({});
   });
 
-  it('* text styles', () => {
-    const item = () => {
-      return (
-        <ItemMock
-          title='Title'
-          titleStyle={{
-            fontSize: 22,
-            lineHeight: 24,
-          }}
-          description='Description'
-          descriptionStyle={{
-            color: 'blue',
-            letterSpacing: 6,
-          }}
-        />
-      );
-    };
+  it('* should be able to call scrollToIndex with ref', () => {
+    // @ts-ignore
+    const componentRef = React.createRef<List>();
 
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
-      />,
+    render(
+      // @ts-ignore
+      <ListMock ref={componentRef}/>,
     );
 
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const { output } = shallow(items[0]);
-
-    expect(output).toMatchSnapshot();
+    expect(componentRef.current.scrollToIndex).toBeTruthy();
+    componentRef.current.scrollToIndex({ index: 0 });
   });
 
-  it('* with icon', () => {
-    const item = () => {
-      return (
-        <ItemMock
-          title='Title'
-          description='Description'
-          icon={icon}
-        />
-      );
-    };
+  it('* should be able to call scrollToIndex with ref', () => {
+    // @ts-ignore
+    const componentRef = React.createRef<List>();
 
-    const icon = (style: StyleType): React.ReactElement<ImageProps> => {
-      return (
-        <Image
-          style={style}
-          source={iconSource}
-        />
-      );
-    };
-
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
-      />,
+    render(
+      // @ts-ignore
+      <ListMock ref={componentRef}/>,
     );
 
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const { output } = shallow(items[0]);
-
-    expect(output).toMatchSnapshot();
+    expect(componentRef.current.scrollToOffset).toBeTruthy();
+    componentRef.current.scrollToOffset({ offset: 0 });
   });
-
-  it('* with accessory', () => {
-    const item = () => {
-      return (
-        <ItemMock
-          title='Title'
-          description='Description'
-          accessory={accessory}
-        />
-      );
-    };
-
-    const accessory = (style: StyleType): React.ReactElement<ImageProps> => {
-      return (
-        <Image
-          style={style}
-          source={iconSource}
-        />
-      );
-    };
-
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
-      />,
-    );
-
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const { output } = shallow(items[0]);
-
-    expect(output).toMatchSnapshot();
-  });
-
-});
-
-describe('@list-item: component checks', () => {
-
-  it('* emits onPress with correct args', async () => {
-    const pressIndex: number = 0;
-
-    const onPress = jest.fn((index: number) => {
-      expect(index).toEqual(pressIndex);
-    });
-
-    const item = () => {
-      return (
-        <ItemMock
-          title='Title'
-          onPress={onPress}
-        />
-      );
-    };
-
-    const component: RenderAPI = render(
-      <Mock
-        data={data}
-        renderItem={item}
-      />,
-    );
-
-    const items: ReactTestInstance[] = component.getAllByType(ListItem);
-    const touchable: ReactTestInstance = items[pressIndex].findByType(TouchableOpacity);
-
-    fireEvent.press(touchable);
-  });
-
 });
