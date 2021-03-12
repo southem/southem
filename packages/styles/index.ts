9a16f376e39bcfec089847a39701755e37c7926c
Patick LUZOLO <eldorplus@gmail.com>
2021-03-12 05:14:36 -0500

    shadow

--- packages/styles/index.ts
+++ packages/styles/index.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 export { default as palette } from './src/palette';
+export { default as shadow } from './src/shadow';
 export { default as styles } from './src/styles';
 export { default as size } from './src/size';
 export { elevations } from './src/elevations';
